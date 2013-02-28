939ce909ce28f527fb7a3c2baaacff30081410af
Michail Bitzes <bitzesmichail@gmail.com>
2013-03-01 00:38:51 +0200

    Changed tests name, added a third test for subtractNumAfter
    Tests renamed as testXwhattotest
    Added comments
    Added third test for subtractNumAfter

--- tests/TestDoublesList.java
+++ tests/TestDoublesList.java
@@ -52,7 +52,7 @@ public class TestDoublesList {
 	}
 
 	/* Test add value starting at index i. Owner: bitzesmichail */
-	static void testaddNumAfter() {
+	static void test1addNumAfter() {
 		DoublesList list = new DoublesList();
 
 		for(int i = 0; i <= 200; i++)
@@ -68,13 +68,25 @@ public class TestDoublesList {
 		for(int i = 100; i <= 200; i++) {
 			assert array[i] == i + 453.0;
 		}
+
+		/* add 0.0 to all - nothing should change */
+		list.addNumAfter(0, 0.0);
+
+		double[] array2 = list.toArray();
+		for(int i = 0; i <= 99; i++) {
+			assert array2[i] == i;
+		}
+
+		for(int i = 100; i <= 200; i++) {
+			assert array2[i] == i + 453.0;
+		}
 	}
 
 	/* It attempts to add at invalid positions, so it should throw exceptions. Owner: bitzesmichail */
-	static void testFaultyIndexInaddNumAfter() {
+	static void test2addNumAfter() {
 		DoublesList list = new DoublesList();
 		try {
-			list.addNumAfter(-1, 453.0); // should throw Exception
+			list.addNumAfter(-1, 453.0); /* illegal index -1 */
 			assert false;
 		}
 		catch(IllegalArgumentException e) {
@@ -83,7 +95,7 @@ public class TestDoublesList {
 		try {
 			list.insert(0, 0.1);
 			list.insert(1, 0.2);
-			list.addNumAfter(5, 0.3); // should throw Exception
+			list.addNumAfter(5, 0.3); /* illegal index 5 */
 			assert false;
 		}
 		catch(IllegalArgumentException e) {
@@ -91,24 +103,26 @@ public class TestDoublesList {
 	}
 
 	/* Test subtract number starting at index. Owner: bitzesmichail */
-	static void testsubtractNumAfter() {
+	static void test1subtractNumAfter() {
 		DoublesList list = new DoublesList();
 
 		for(int i = 0; i <= 200; i++)
 			list.insert(i, i);
 
+		/* make all elements zero */
 		for(int i = 200; i >= 0; i--)
 			list.subtractNumAfter(i, i);
 
+		/* the array should be empty */
 		double[] array = list.toArray();
 		assert array.length == 0;
 	}
 
 	/* It attempts to insert at invalid positions, so it should throw exceptions. Owner: bitzesmichail */
-	static void testFaultyIndexInsubtractNumAfter() {
+	static void test2subtractNumAfter() {
 		DoublesList list = new DoublesList();
 		try {
-			list.subtractNumAfter(-1, 453.0); // should throw Exception
+			list.subtractNumAfter(-1, 453.0); /* illegal index -1 */
 			assert false;
 		}
 		catch(IllegalArgumentException e) {
@@ -117,11 +131,26 @@ public class TestDoublesList {
 		try {
 			list.insert(0, 0.1);
 			list.insert(1, 0.2);
-			list.subtractNumAfter(5, 0.3); // should throw Exception
+			list.subtractNumAfter(5, 0.3); /* illegal index 5 */
 			assert false;
 		}
 		catch(IllegalArgumentException e) {
 		}
+	}
+
+	static void test3subtractNumAfter() {
+		DoublesList list = new DoublesList();
+
+		for(int i = 0; i <= 200; i++)
+			list.insert(i, i);
+
+		/* remove 0.0 from all elements starting at 1 */
+		list.subtractNumAfter(1, 0.0);
+
+		/* nothing should have changed */
+		double[] array = list.toArray();
+		for(int i = 0; i <= 200; i++)
+			assert array[i] == i;
 	}
 
 	/* Runs all tests */
@@ -129,11 +158,12 @@ public class TestDoublesList {
 		testInsertion();
 		testFaultyInsertion();
 		testManyInsertions();
-		testaddNumAfter();
-		testFaultyIndexInaddNumAfter();
-		testsubtractNumAfter();
-		testFaultyIndexInsubtractNumAfter();
-		
+		test1addNumAfter();
+		test2addNumAfter();
+		test1subtractNumAfter();
+		test2subtractNumAfter();
+		test3subtractNumAfter();
+
 		System.out.println("All tests run OK.");
 	}
 
